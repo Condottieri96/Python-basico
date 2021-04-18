57d28964e4b52f67a0fda2988769be665a8c7b47
Thales Martim <79290513+Condottieri96@users.noreply.github.com>
2021-04-18 18:41:35 -0300

    Update conta.py

--- exercicio banco/conta.py
+++ exercicio banco/conta.py
@@ -1,13 +1,17 @@
+#criacao do objeto conta, para isso se importa os a funcao cliente do arquivo cliente
 from cliente import cliente
+#criaçao do objeto conta e delimitacao dos seus atributos
 class conta:
     def __init__(self, saldo, limite, cliente):
         self.saldo = saldo
         self.limite = limite
         self.cliente = cliente
+    #definicao do funcao sacar dentro do objeto conta
     def Sacar (self, sacar):
         if sacar > self.saldo + self.limite:
             print ('saldo insufuciente para saque, consulte seu saldo')
         else:
             self.saldo -= sacar
+    #definicao da funcao depositar dentro do objeto conta
     def Depositar (self, depositar):
         self.saldo += depositar
