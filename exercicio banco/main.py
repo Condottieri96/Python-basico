b74df4200e532bb63a46d993b198339e88a17520
Thales Martim <79290513+Condottieri96@users.noreply.github.com>
2021-04-18 18:37:27 -0300

    Update main.py

--- exercicio banco/main.py
+++ exercicio banco/main.py
@@ -1,8 +1,13 @@
+'''
+exercicio para gerenciar dados de clientes em um banco, exercicio para introducao a orientacao a objeto
+'''
+# importando objeto das bibliotecas criadas "cliente" e "conta"
 from conta import conta
 from cliente import cliente
+# criando conta e cliente para o exercicio
 cliente1 = cliente ('joao', 42, 2045)
 conta1 = conta (40, 50, cliente1)
-
+#testes para ver se os objetos e funcoes estao funcionando como previsto
 print (conta1.saldo)
 conta1.Depositar (50)
 print (conta1.saldo)
