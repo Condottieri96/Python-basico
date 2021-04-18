3275b6b447e0d1325abb1d2845f5d0b58a37cb7a
Thales Martim <79290513+Condottieri96@users.noreply.github.com>
2021-04-18 17:29:15 -0300

    Update exercicio-exercito.py

--- exercicio-exercito.py
+++ exercicio-exercito.py
@@ -1,7 +1,13 @@
-idade = float (input ('qual sua idade? '))
+'''
+neste exercicio, simulei um programa para avaliar a apitidao de possiveis candidatos a entrar no exercito, porém sem o uso dos criterios reais, nele pude trabalhar
+com operações logicas, comparações, bem como diferentes tipos de tipos de dados.
+'''
+#primeiro solicito ao usuario os valores das variaveis utilizadas com o imput, e as converto de string para boolean, float e integer
+idade = int (input ('qual sua idade? '))
 peso = float (input ('qual o seu peso? '))
 altura = float (input ('qual sua altura? '))
 homem = bool (input ('voce é homem? '))
+#então retorno para o usuario com print a resposta adeguada segundo os dados fornecidos e o uso do if else e and para a comparacao
 if idade > 18 and idade < 30 and peso >= 60 and peso < 80 and altura >= 1.70 and homem == True:
     print ('voce esta apto a entrar no exercito')
 else:
